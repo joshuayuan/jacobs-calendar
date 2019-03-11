b05aa7626bb5b6f22b385b8b15c7097cd83b7ed7
Joshua Yuan <joshua43210@gmail.com>
2019-03-11 15:37:45 -0700

    fill dictionaries with time deltas

---
+++ script.py
@@ -0,0 +1,39 @@
+import pandas
+import utils
+
+df = pandas.read_csv('3-1-19_3-10-19.csv', parse_dates=['Start time', 'Finish time'], usecols=[0, 1, 2]) 
+
+# Only look at cutter, and respective start/stop times
+df.columns = ["Cutter", "Start", "Stop"] 
+
+# Remap cutter names to be more simple
+df["Cutter"] = df["Cutter"].map({"Red laser cutter 48x24 in": "Red", "Blue laser cutter 32x18 in": "Blue", "Yellow laser cutter 32x18 in": "Yellow", "Green laser cutter 32x18 in": "Green"})
+
+# group by cutters, then by start times within each cutter grouping
+df = df.sort_values(by=["Cutter", "Start"]) 
+
+# create dictionaries. Consider using default dicts later
+reds = {}
+blues = {}
+greens = {}
+yellows = {}
+
+for index, cutter, start, stop in df.itertuples():
+    date = utils.get_date_str(start)
+    # Initialize dictionary with default time deltas of 0
+    if date not in reds or date not in blues or date not in greens or date not in yellows:
+        reds[date] = pandas.Timedelta(0)
+        blues[date] = pandas.Timedelta(0)
+        greens[date] = pandas.Timedelta(0)
+        yellows[date] = pandas.Timedelta(0)
+
+    # add time deltas to the respective dicts
+    if cutter == "Red":
+        reds[date] += stop - start
+    elif cutter == "Blue":
+        blues[date] += stop - start
+    elif cutter == "Green":
+        greens[date] += stop - start
+    elif cutter == "Yellow":
+        yellows[date] += stop - start
+
---
+++ utils.py
@@ -0,0 +1,2 @@
+def get_date_str(timestamp):
+    return str(timestamp.month) + "-" + str(timestamp.day) + "-" + str(timestamp.year)
