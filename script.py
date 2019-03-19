39f969c5055150bf318ef66e10a20bd353d78266
Joshua Yuan <joshua43210@gmail.com>
2019-03-19 00:57:18 -0700

    add multiple lines

--- script.py
+++ script.py
@@ -2,7 +2,8 @@ import pandas
 import utils
 import matplotlib.pyplot as plt
 
-df = pandas.read_csv('3-1-19_3-10-19.csv', parse_dates=['Start time', 'Finish time'], usecols=[0, 1, 2]) 
+# df = pandas.read_csv('data/1-1-19_3-18-19.csv', parse_dates=['Start time', 'Finish time'], usecols=[0, 1, 2])
+df = pandas.read_csv('data/3-1-19_3-10-19.csv', parse_dates=['Start time', 'Finish time'], usecols=[0, 1, 2]) 
 
 # Only look at cutter, and respective start/stop times
 df.columns = ["Cutter", "Start", "Stop"] 
@@ -21,6 +22,7 @@ greens = {}
 yellows = {}
 
 for index, cutter, start, stop in df.itertuples():
+    print(start.day_name())
     date = utils.get_date_str(start)
     # Initialize dictionary with default time deltas of 0
     if date not in reds or date not in blues or date not in greens or date not in yellows:
@@ -39,17 +41,38 @@ for index, cutter, start, stop in df.itertuples():
     elif cutter == "Yellow":
         yellows[date] += stop - start
 
-keys = []
-values = []
-for key in sorted(reds.keys()):
-    keys.append(key)
-    values.append(reds[key].seconds)
+# In the future do something with matrix calculations.
 
-print(keys)
-print(values)
-x = range(0,10)
-plt.plot(x, values)
-plt.xticks(x, keys)
+red_keys = []
+red_values = []
+for key in sorted(reds.keys()):
+    red_keys.append(key)
+    red_values.append(reds[key].seconds)
+blue_keys= []
+blue_values = []
+for key in sorted(reds.keys()):
+    blue_keys.append(key)
+    blue_values.append(blues[key].seconds)
+
+yellow_keys = []
+yellow_values = []
+for key in sorted(reds.keys()):
+    yellow_keys.append(key)
+    yellow_values.append(yellows[key].seconds)
+
+green_keys = []
+green_values = []
+for key in sorted(reds.keys()):
+    green_keys.append(key)
+    green_values.append(greens[key].seconds)
+
+
+x = range(0, len(red_values))
+plt.plot(x, red_values, 'r')
+plt.plot(x, yellow_values, 'y')
+plt.plot(x, blue_values, 'b')
+plt.plot(x, green_values, 'g')
+plt.xticks(x, red_keys)
 
 plt.show()
 
