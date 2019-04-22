c2fbb049e91a2a8b2b6b2fee74730701a86e764c
Joshua Yuan <joshua43210@gmail.com>
2019-04-22 14:34:48 -0700

    date input to grab all appointments from that time

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from SuperSaaS import Client
 from datetime import datetime
 import utils
-import tkinter as tk
+import Tkinter as tk
+import matplotlib.pyplot as plt
 from tkcalendar import Calendar
+import pandas
 
 
 api_filename = "API_KEY"
@@ -11,10 +13,80 @@ Client.instance().configure(
     api_key = utils.get_file_contents(api_filename)
 )
 
+while 1:
+    user_date = raw_input("Enter date with the format mm-dd-yyyy:\t")
+    try:
+        starting_date = datetime.strptime(user_date, '%m-%d-%Y')
+        break
+    except ValueError as e:
+        print(e)
+ll = Client.instance().appointments.list(schedule_id=289549, start_time=starting_date)
+reds = {}
+blues = {}
+greens = {}
+yellows = {}
 
-
-ll = Client.instance().appointments.list(schedule_id=289549, start_time=datetime.now(), limit=5)
-print(ll)
+i = 0
 for l in ll:
-    print(l.start, l.finish, l.res_name)
+    i += 1
+    name = l.res_name.split()[0]
+    stop = datetime.strptime(l.finish, '%Y-%m-%dT%H:%M')
+    start= datetime.strptime(l.start, '%Y-%m-%dT%H:%M')
+    date = utils.get_date_str(stop)
+
+    if date not in reds or date not in blues or date not in greens or date not in yellows:
+        reds[date] = pandas.Timedelta(0)
+        greens[date] = pandas.Timedelta(0)
+        blues[date] = pandas.Timedelta(0)
+        yellows[date] = pandas.Timedelta(0)
+
+    if name  == "Red":
+        # red laser cutter
+        reds[date] += stop - start
+    elif name == "Blue":
+        # blue laser cutter
+        blues[date] += stop - start
+    elif name == "Green":
+        # green laser cutter
+        greens[date] += stop - start
+    elif name == "Yellow":
+        # yellow laser cutter
+        yellows[date] += stop - start
+print(i)
+
+red_keys = []
+red_values = []
+for key in sorted(reds.keys()):
+    red_keys.append(key)
+    red_values.append(reds[key].seconds)
+blue_keys= []
+blue_values = []
+for key in sorted(reds.keys()):
+    blue_keys.append(key)
+    blue_values.append(blues[key].seconds)
+
+yellow_keys = []
+yellow_values = []
+for key in sorted(reds.keys()):
+    yellow_keys.append(key)
+    yellow_values.append(yellows[key].seconds)
+
+green_keys = []
+green_values = []
+for key in sorted(reds.keys()):
+    green_keys.append(key)
+    green_values.append(greens[key].seconds)
+
+
+x = range(0, max(len(red_values), len(blue_values), len(yellow_values), len(green_values)))
+plt.plot(x, red_values, 'r')
+plt.plot(x, yellow_values, 'y')
+plt.plot(x, blue_values, 'b')
+plt.plot(x, green_values, 'g')
+ax = plt.axes()
+ax.xaxis.set_major_locator(plt.MultipleLocator(base=7.0))
+ax.xaxis.set_minor_locator(plt.MultipleLocator(base=1.0))
+ax.xaxis.set_major_formatter(plt.NullFormatter())
+
+plt.show()
 
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 def get_date_str(timestamp):
     return str(timestamp.month) + "-" + str(timestamp.day) + "-" + str(timestamp.year)
+
 def get_file_contents(filename):
     try:
         with open(filename, 'r') as f:
